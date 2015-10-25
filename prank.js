55bff22cb0f43371eaabd8b436283904a903e451
davpal <paluchdaw@gmail.com>
2015-10-25 21:14:42 +0000

    Use bootstrap

--- prank.js
+++ prank.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  	$('#container').on('click', '#button', function(e){
-    	var source = $('#source').val();
-		$('#source').val(source.replace(";", "\u037E"));
+  	$('.container').on('click', '#button', function(e){
+    	var source = $('.source').val();
+		$('.source').val(source.replace(";", "\u037E"));
 		print_info();
   	});
-  	$('#container').on('change keyup paste', '#source', hide_info);
+  	$('.container').on('change keyup paste', '.source', hide_info);
 });
 
 function print_info(){
-	$('<h2 id="info">Copy results and piss off the programmers!</h2>').insertBefore('#source');
+	$('<div id="info" class="alert alert-success">Copy results and piss off the programmers!</div>').insertBefore('.source');
 	$('#info').hide().fadeIn('2000');
 }
 
